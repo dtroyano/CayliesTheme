a983a95da9e4617517529964da5391f22b78f84d
Daniel Troyano <dtroyano@cox.net>
2013-03-26 22:50:02 -0500

    post added

--- style.jquery.js
+++ style.jquery.js
@@ -1,8 +1,15 @@
-$(function (){
-	$(".comments").hover(function()
-		$('.comments').animate({
-			width: '80px',
-			height: '80px'
-		});
+function commentBubble(speed){
+	$(".comments").mouseover(function(){
+		$('.comments').stop().animate({width: '75px',height: '75px'},speed, 'swing');
+		$('.commentnum').stop().animate({fontSize: '1.6em'},speed, 'linear');
 	});
+	$(".comments").mouseout(function(){
+		$('.comments').stop().animate({width: '70px',height: '70px'},speed, 'swing');
+		$('.commentnum').stop().animate({fontSize: '1.5em'},speed, 'linear');
+	});
+}
+
+
+$(function (){
+	commentBubble(500);
 });
